562fe9abe71bb468973fa621a20f94818e3d714f
Dadaisuk1 <darwindarryljean.largoza@cit.edu>
2026-05-02 15:49:22 +0800

    fix: delete from the start

--- src/components/Button/index.ts
+++
@@ -1,5 +0,0 @@
-export { DarkPill } from "./DarkPill";
-export { LargePill } from "./LargePill";
-export { LightPill } from "./LightPill";
-export { OutlinedPill } from "./OutlinedPill";
-export { CircularPlay } from "./CircularPlay";
--- src/components/Layout/index.ts
+++
@@ -1,4 +0,0 @@
-export { Sidebar } from "./Sidebar";
-export { MainContent } from "./MainContent";
-export { NowPlayingBar } from "./NowPlayingBar";
-export { AppLayout } from "./AppLayout";
